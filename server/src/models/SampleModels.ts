53c20f1d2f218a802f123e9ed20af3f8dc258629
Eva Villarroya <eva.villarroya@thoughtworks.com>
2022-01-24 10:45:42 +0100

    Adding Jest library for testing

--- server/src/models/SampleModels.ts
+++ server/src/models/SampleModels.ts
@@ -13,4 +13,11 @@ export class SampleModels {
 
   @Field(() => String)
   disease: boolean;
+
+  constructor(id:string, healthFacility:string, microscopist:string, disease:boolean) {
+    this.id = id
+    this.healthFacility = healthFacility
+    this.microscopist = microscopist
+    this.disease = disease
+  }
 }
---
+++ server/test/models/SampleModels.test.ts
@@ -0,0 +1,4 @@
+test('Checks Jest is setup', () => {
+    const var_name = new Number(1)
+    expect(var_name.valueOf()).toBe(1)
+});
