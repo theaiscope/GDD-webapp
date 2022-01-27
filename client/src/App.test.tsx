61ba8156f90a3c7a0aaa002f951e1ef2139d3c5a
Fergus Orbach <fergio@Ferguss-MBP.home>
2022-01-27 18:01:37 +0100

    set up github actions to test client

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders an image', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const image = screen.getByRole("img");
+  expect(image).toBeInTheDocument();
 });
