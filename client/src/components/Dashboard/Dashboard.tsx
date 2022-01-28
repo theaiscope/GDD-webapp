dc7036a66fa66c01b8b0eddc18a3771545c835e9
Fergus Orbach <forbach@thoughtworks.com>
2022-01-28 18:31:01 +0100

    deploy site via github actions

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -5,5 +5,6 @@ import { LogOut } from "../../Auth";
 export const Dashboard = (): ReactElement =>
   <div>
     <p>I am the Dashboard - please implement me</p>
+    <p>clicking and zooming</p>
     <LogOut/>
   </div>
