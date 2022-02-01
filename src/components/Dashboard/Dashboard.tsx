51e20c5aa9d022441e01dd5f0863ee67173164e8
Fergus Orbach <forbach@thoughtworks.com>
2022-02-01 12:27:41 +0100

    add dashboard styling

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement } from 'react'
+import styles from './Dashboard.module.css'
 
 export const Dashboard = (): ReactElement => (
-  <div>
+  <div className={styles.container}>
     <p>I am the Dashboard - please implement me</p>
   </div>
 )
