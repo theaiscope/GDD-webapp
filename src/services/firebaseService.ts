c8a11b302733aa8852f9f5375ad8561cd8592483
Gonzalo Cao <gonzalo.vazquez1@thoughtworks.com>
2022-03-13 12:50:46 +0100

    Get random images from firebase now working.

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -16,4 +16,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const firebaseService = getAuth(app)
-export const databaseClient = getDatabase(app)
+export const databaseClient = getFirestore(app)
