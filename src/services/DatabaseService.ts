e0bc480567d2026afbe3dc0c7e407960ea064f82
Gonzalo Cao <gonzalo.vazquez1@thoughtworks.com>
2022-03-13 12:50:46 +0100

    Get random images from firebase now working.

--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -1,6 +1,18 @@
 import {databaseClient} from "./firebaseService";
-import {query, ref} from "firebase/database";
+import {collection, query, getDocs} from "firebase/firestore";
+import {ImageContainer} from "react-canvas-draw";
 
-export async function GetDataAsObject(container: string): Promise<string> {
-  return query(ref(databaseClient, container)).toJSON()
+export async function GetDataAsObject(container: string): Promise<ImageContainer[]> {
+    const array : ImageContainer[] = []
+
+    const samples = collection(databaseClient, container);
+    const q = query(samples)
+    const snapshot = await getDocs(q);
+
+    snapshot.forEach((doc) => {
+        array.push(<ImageContainer>doc.data())
+        console.log(doc.data())
+    })
+
+    return array
 }
--- src/services/ImageRepositoryService.ts
+++ src/services/ImageRepositoryService.ts
@@ -1,29 +1,39 @@
 import {getStorage, ref, getDownloadURL} from "firebase/storage";
-import {ImageDimensions} from "react-canvas-draw";
+import {ImageContainer, ImageDimensions} from "react-canvas-draw";
 
-export async function getImage(): Promise<string> {
-  const storage = getStorage();
-  const url = await getDownloadURL(ref(storage, '00ad4093-111e-4900-9dee-10abcc02abb7/image_0.jpg'));
+export async function getImage(imageArray: ImageContainer[]): Promise<string> {
+    const storage = getStorage()
+    const flattenedArray = [...imageArray]
 
-  return url;
+    const sampleIndex = getArrayIndex(flattenedArray.length)
+    console.log(sampleIndex)
+    const imageIndex = getArrayIndex(flattenedArray[sampleIndex].images.length)
+    const reference = ref(storage, flattenedArray[sampleIndex].location + "/" + flattenedArray[sampleIndex].images[imageIndex].name)
+    const url = await getDownloadURL(reference);
+
+    return url;
 }
 
-export async function getImageDimensions(url: string) : Promise<ImageDimensions> {
-  const metadata = await getMeta(url)
+export async function getImageDimensions(url: string): Promise<ImageDimensions> {
+    const metadata = await getMeta(url)
 
-  return {
-    width: metadata.width,
-    height: metadata.height
-  }
+    return {
+        width: metadata.width,
+        height: metadata.height
+    }
+}
+
+function getArrayIndex(itemsInArray: number) {
+    return Math.floor(Math.random() * itemsInArray);
 }
 
 function getMeta(url: string) {
-  return new Promise<HTMLImageElement>((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = () => reject();
-    img.src = url;
-  });
+    return new Promise<HTMLImageElement>((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = () => reject();
+        img.src = url;
+    });
 }
 
 
