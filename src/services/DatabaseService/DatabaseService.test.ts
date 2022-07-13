c6605076c29ed62f867c36c611b26017b7fed461
ana.lopezestrella <ana.lopezestrella@thoughtworks.com>
2022-07-13 12:04:46 +0200

    Make it possible to run tests with firebase emulator

--- src/services/DatabaseService/DatabaseService.test.ts
+++ src/services/DatabaseService/DatabaseService.test.ts
@@ -1,22 +1,59 @@
 /** @jest-environment node */
+import { connectFirestoreEmulator, QueryConstraint, where } from 'firebase/firestore';
+import { signInWithEmailAndPassword, connectAuthEmulator } from "firebase/auth";
 import { getDocuments } from './DatabaseService';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-import { firebaseService } from '../firebaseService';
+import { firebaseService, databaseClient } from '../firebaseService';
 
 describe('Database Service', () => {
+
+    beforeAll(() => {
+        connectFirestoreEmulator(databaseClient, 'localhost', 8080);
+        connectAuthEmulator(firebaseService, 'http://localhost:9099');
+    });
+
+    beforeEach(async () => {
+        await signInWithEmailAndPassword(firebaseService, 'some@user.com', 'someuser');
+    });
+
     describe('getDocuments', () => {
-        it.skip('should retrieve all documents of specified collection name without query string', async () => {
-            const aux = await createUserWithEmailAndPassword(firebaseService, 'some@email.com', 'somepassword');
-            const aux1 = await signInWithEmailAndPassword(firebaseService, 'some@email.com', 'somepassword');
+        const fakeDocuments = [
+            {
+                markedAsInvalid: false,
+                sampleLocation: '00ad4093-111e-4900-9dee-10abcc02abb7',
+                name: 'image_0.jpg',
+                masks: [{ name: 'masks_0.png', uploadedBy: 'microscopists/FvEb3AHYnlGubLvKJyWH' }],
+                createdOn: { nanoseconds: 851000000, seconds: 1657626586 },
+                isCompleted: false,
+                labellers: ['FvEb3AHYnlGubLvKJyWH'],
+                sampleReference: 'samples/rGjt8jcwSQzGRVcViJbQ'
+            },
+            {
+                markedAsInvalid: false,
+                sampleLocation: '00ad4093-111e-4900-9dee-10abcc02abb7',
+                name: 'image_1.jpg',
+                masks: [],
+                createdOn: { nanoseconds: 494000000, seconds: 1657635604 },
+                isCompleted: false,
+                labellers: ['FvEb3AHYnlGubLvKJyWH'],
+                sampleReference: 'samples/rGjt8jcwSQzGRVcViJbQ'
+            }
+        ];
 
-            const expectedDocuments = { enabled: false };
+        it('should retrieve all documents of specified collection name without query string', async () => {
+            const actualDocuments = await getDocuments('images');
 
-            const actualDocuments = await getDocuments('microscopists');
+            expect(actualDocuments).toEqual(fakeDocuments);
+        });
 
-            expect(actualDocuments).toBe(expectedDocuments);
-        })
-    })
-})
+        it('should retrieve all documents of specified collection name using query string', async () => {
+            const constraints: QueryConstraint[] = [where('name', '==', 'image_1.jpg')];
+            const actualDocuments = await getDocuments('images', constraints);
+
+            const expectedDocuments = [fakeDocuments[1]];
+            expect(actualDocuments).toEqual(expectedDocuments);
+        });
+    });
+});
 
 
 
--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -1,6 +1,6 @@
-import { initializeApp } from 'firebase/app'
-import { connectAuthEmulator, getAuth } from 'firebase/auth'
-import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,16 +11,10 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-}
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
-export const databaseClient = getFirestore(app)
-if (firebaseConfig.databaseURL?.includes('localhost')) {
-  connectFirestoreEmulator(databaseClient, 'localhost', 8080);
-}
+const app = initializeApp(firebaseConfig);
+export const databaseClient = getFirestore(app);
 
-export const firebaseService = getAuth(app)
-if (firebaseConfig.databaseURL?.includes('localhost')) {
-  connectAuthEmulator(firebaseService, 'http://localhost:9099');
-}
+export const firebaseService = getAuth(app);
