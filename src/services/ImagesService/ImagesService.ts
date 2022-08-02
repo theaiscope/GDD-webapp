6c90e00e94d51fb8c585781e07299103d52e5b21
Marcos Jordao <marcos.jordao@thoughtworks.com>
2022-08-02 17:27:06 +0200

    Add SkipImage function

--- src/services/ImagesService/ImagesService.ts
+++ src/services/ImagesService/ImagesService.ts
@@ -1,7 +1,11 @@
 import { orderBy, QueryConstraint, where } from 'firebase/firestore'
+import { httpsCallable } from 'firebase/functions'
 import { CREATED_ON, IS_COMPLETED } from '../../assets/services/queryConstants'
 import { Image } from '../../model/image'
+import { CloudFunctions } from '../cloudFunctions'
 import { getDocuments } from '../DatabaseService/DatabaseService'
+import { functionsInstance } from '../firebaseService'
+import { SkipImageRequest, SkipImageResponse } from './api/SkipImageApi'
 
 export async function fetchImages(userUid: string): Promise<Image[]> {
   const imagesResult: Image[] = []
@@ -13,4 +17,16 @@ export async function fetchImages(userUid: string): Promise<Image[]> {
   })
 
   return imagesResult
+}
+
+export async function skipImage(imageId: string): Promise<SkipImageResponse> {
+  const skipImageFunction = httpsCallable<SkipImageRequest, SkipImageResponse>(
+    functionsInstance,
+    CloudFunctions.SKIP_IMAGE,
+  )
+
+  const requestData = { imageId }
+  const response = await skipImageFunction(requestData)
+
+  return response.data as SkipImageResponse
 }
---
+++ src/services/ImagesService/api/SkipImageApi.ts
@@ -0,0 +1,9 @@
+export interface SkipImageRequest {
+  imageId: string
+}
+
+export interface SkipImageResponse {
+  message: string
+  imageId: string
+  labellerId: string
+}
