5fe91299b3cd68f97711dd557ff81c62033ee502
Marcos Jordao <marcos.jordao@thoughtworks.com>
2022-11-02 16:49:35 +0100

    Refactor Spinner to receive state by prop

--- src/components/LoadingSpinner/LoadingSpinner.test.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.test.tsx
@@ -1,29 +1,16 @@
-import { render, screen } from '@testing-library/react'
 import React from 'react'
+import { render, screen } from '@testing-library/react'
 import { LoadingSpinner } from './LoadingSpinner'
-import * as LoadingHook from '../../hooks/Loading/LoadingHook'
 
 describe('LoadingSpinner', () => {
   it('should display a ProgressBar when is loading', () => {
-    jest.spyOn(LoadingHook, 'useLoading').mockReturnValue({
-      isLoading: true,
-      showLoading: () => jest.fn(),
-      hideLoading: () => jest.fn,
-    })
-
-    render(<LoadingSpinner></LoadingSpinner>)
+    render(<LoadingSpinner isLoading={true}></LoadingSpinner>)
 
     expect(screen.getByRole('progressbar', { hidden: true })).toBeVisible()
   })
 
   it('should not display a ProgressBar when is not loading', () => {
-    jest.spyOn(LoadingHook, 'useLoading').mockReturnValue({
-      isLoading: false,
-      showLoading: () => jest.fn(),
-      hideLoading: () => jest.fn,
-    })
-
-    render(<LoadingSpinner></LoadingSpinner>)
+    render(<LoadingSpinner isLoading={false}></LoadingSpinner>)
 
     expect(screen.getByRole('progressbar', { hidden: true })).not.toBeVisible()
   })
--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,14 +1,13 @@
 import React, { ReactElement } from 'react'
 import { Backdrop, CircularProgress } from '@material-ui/core'
 import styles from './LoadingSpinner.module.css'
-import { useLoading } from '../../hooks/Loading/LoadingHook'
 
-export const LoadingSpinner = (): ReactElement => {
-  const { isLoading } = useLoading()
-
-  return (
-    <Backdrop open={isLoading} className={styles.progressBackdrop} aria-label="Loading Spinner">
-      <CircularProgress />
-    </Backdrop>
-  )
+type Props = {
+  isLoading: boolean
 }
+
+export const LoadingSpinner = (props: Props): ReactElement => (
+  <Backdrop open={props.isLoading} className={styles.progressBackdrop} aria-label="Loading Spinner">
+    <CircularProgress />
+  </Backdrop>
+)
